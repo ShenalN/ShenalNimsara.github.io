9aff7d026a65b1f0df66b71a40f8442053029780
ShenalN <86877221+ShenalN@users.noreply.github.com>
2021-10-11 10:09:14 +0530

    Initial commit with basic files

---
+++ main.js
@@ -0,0 +1,26 @@
+var menuBtn = document.getElementById("menuBtn");
+
+var sideNav = document.getElementById("sideNav");
+
+menuBtn.addEventListener("click", menuBar);
+
+sideNav.style.right = -400 + "px";
+
+  function menuBar() {
+    if (sideNav.style.right == -400 + "px") {
+      sideNav.style.right = 0 + "px";
+      menuBtn.src = "images/menuclose.png";
+    }else {
+      sideNav.style.right = -400 + "px";
+      menuBtn.src = "images/menu.png";
+    }
+  }
+  
+  
+//pre loader
+
+
+window.addEventListener("load", function() {
+  document.querySelector(".loader").classList.add("hide");
+});
+  
